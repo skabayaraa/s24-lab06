db5e65b8833c0b95dcc76e6f948dcc301dabaf38
Enkhsaruul Erdenebayar <skabayaraa208@gmail.com>
2025-03-18 14:26:12 +0800

    lab_07_2
    lab_07_2

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -4,37 +4,37 @@ public class DelegationSortedIntList implements IntegerList {
     private SortedIntList sortedIntList = new SortedIntList();
     private int totalAdded = 0;
 
-    // Delegate the add method to the sortedIntList
+    
     @Override
     public boolean add(int element) {
-        sortedIntList.add(element);  // Delegate to SortedIntList
-        totalAdded++;  // Increment the counter for added elements
-        return true;  // Return true as the element is successfully added
+        sortedIntList.add(element);  
+        totalAdded++;  
+        return true; 
     }
 
-    // Delegate the addAll method to the sortedIntList
+    
     @Override
     public void addAll(int[] elements) {
-        sortedIntList.addAll(elements);  // Delegate to SortedIntList
-        totalAdded += elements.length;  // Add the number of elements added
+        sortedIntList.addAll(elements);  
+        totalAdded += elements.length;  
     }
 
-    // Method to get the total number of added elements
+    
     @Override
     public int getTotalAdded() {
         return totalAdded;
     }
 
-    // Delegate the size method to SortedIntList
+   
     @Override
     public int size() {
-        return sortedIntList.size();  // Return the size of the SortedIntList
+        return sortedIntList.size();  
     }
 
-    // Delegate the get method to SortedIntList
+   
     @Override
     public int get(int i) {
-        return sortedIntList.get(i);  // Return the element at the given index
+        return sortedIntList.get(i);  
     }
 }
 
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/Main.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/Main.java
@@ -9,20 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /** 
-         * This is a sandbox to experiment in.
-         * Uncomment the following code to play with your implementation 
-         * Select all and (CTRL + /) or (COMMAND + /)
-         * Feel free to write more to test your implementation. 
-         */
-
-        IntegerList list1;
+IntegerList list1;
         IntegerList list2;
 
         list1 = new DelegationSortedIntList();  // Initialize with DelegationSortedIntList
         list2 = new InheritanceSortedIntList(); // Initialize with InheritanceSortedIntList
 
-        // add 5 elements to our first list.
         list1.add(1);
         list1.add(3);
         list1.add(2);
@@ -30,17 +22,16 @@ public class Main {
         list1.add(2);
 
         printList(list1);
-        System.out.println("Total Added (list1): " + list1.getTotalAdded());  // Print total added
+        System.out.println("Total Added (list1): " + list1.getTotalAdded());  
 
-        // add 2 elements to a second list.
         list2.add(3);
         list2.add(0);
 
-        // Add all elements from list1 to list2 (addAll expects an array of elements)
-        list2.addAll(new int[]{1, 3, 2, 4, 2});  // Pass the elements from list1 directly as an array
+       
+        list2.addAll(new int[]{1, 3, 2, 4, 2});  
 
         printList(list2);
-        System.out.println("Total Added (list2): " + list2.getTotalAdded());  // Print total added
+        System.out.println("Total Added (list2): " + list2.getTotalAdded());  
     }
 
     /**
